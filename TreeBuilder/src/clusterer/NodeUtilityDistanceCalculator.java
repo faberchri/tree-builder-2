3f329c904b091617fed611e1c2f45d91bb53c084
alessandramacri <alessandra.macri@hotmail.com>
2012-11-25 20:10:48 +0100

    Added utility classes to allow creation of a node for utility calculation only. This node may not alter the tree.

--- TreeBuilder/src/clusterer/NodeUtilityDistanceCalculator.java
+++ TreeBuilder/src/clusterer/NodeUtilityDistanceCalculator.java
@@ -1,12 +1,16 @@
 package clusterer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class NodeUtilityDistanceCalculator implements INodeDistanceCalculator {
 
 	//TODO: When merging more than 2 nodes, utility can't be simply translated into distance, since utility
-	// depends on the merged node itself
+	// depends on which nodes are merged
 	
+	// Calculates distance of two nodes based on Cobweb utility function
 	public double calculateDistance(INode n1, INode n2) {
-		// Calculates distance of two nodes based on Cobweb utility function
 		
 		double pClass = 0; //Probability of the class
 		double pAttrInClass = 0; //Probability of the attribute value, given membership in class
@@ -15,11 +19,28 @@ public class NodeUtilityDistanceCalculator implements INodeDistanceCalculator {
 		
 		double utility = 0; //The utility metric to be calculated
 		double distance = 0;
-		//1. Merge n1 and n2	-> New merge function needed?
-		//2. Calculate utility for the new node
-		utility = (pClass * pAttrInClass * pAttrInData) / k;
+		
+		List<INode> nodes = new ArrayList(); 
+		nodes.add(n1);
+		nodes.add(n2);
+		
+		UtilityNode tempNode = new UtilityNode(); //Temporary node for utility calculation
+		
+		//1. Merge n1 and n2	
+		
+		UtilityNodeFactory fac = new UtilityNodeFactory();
+		tempNode = (UtilityNode) fac.createCalculationNode(nodes);
+		
+		//2. TODO: Calculate utility for the new node
+		if(k != 0){
+			utility = (pClass * pAttrInClass * pAttrInData) / k;	
+		}
+		
 		//3. Calculate distance from utility (Nodes with high utility should have low distance)
-		distance = 1/utility;
+		if(utility !=0)
+			distance = 1/utility;
+		else
+			distance = Double.MAX_VALUE;
 		
 		return distance;
 	}
