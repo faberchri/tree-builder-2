8230b37f11390b869e70f833eb0de4be5e448e2c
danielhegglin <danielhegglin@gmail.com>
2012-11-25 23:54:27 +0100

    Added functionality to handle multiple findings.. not tested yet

--- TreeBuilder/src/modules/MultipleClosestNodesSearcher.java
+++ TreeBuilder/src/modules/MultipleClosestNodesSearcher.java
@@ -12,25 +12,37 @@ public class MultipleClosestNodesSearcher implements IClosestNodesSearcher {
 
 	@Override
 	public List<INode> getClosestNodes(Set<INode> openNodes) {
+		
 		long time = System.currentTimeMillis();
 		double closestDistance = Double.MAX_VALUE;
 		List<INode> closestNodes = new ArrayList<INode>();
 		Set<INode> subSet = new HashSet<INode>(openNodes);
-		for (INode node : openNodes) {
-			subSet.remove(node);
-			for (INode node2 : subSet) {
-				double tmpDi = node.getDistance(node2);
-				if (tmpDi < closestDistance){
-					closestDistance = tmpDi;
+		
+		// Add all Nodes with closest Distance to closestNodes List
+		for (INode openNode : openNodes) {
+			subSet.remove(openNode);
+			for (INode subSetNode : subSet) {
+				double tmpDistance = openNode.getDistance(subSetNode);
+				
+				// Found closer distance -> all previous found nodes don't matter anymore
+				if (tmpDistance < closestDistance){
+					closestDistance = tmpDistance;
 					closestNodes.clear();
-					closestNodes.add(node);
-					closestNodes.add(node2);
+					closestNodes.add(openNode);
+					closestNodes.add(subSetNode); // is this good?
+					System.out.println("Found new closest Distance beetween "+openNode+", "+subSetNode+" ("+tmpDistance+")");
 				}
-//				System.out.println("calculation: "+node+", "+node2+" ("+tmpDi+")");
+				
+				// Found similar distance -> add node to collection
+				if(tmpDistance == closestDistance){
+					closestNodes.add(openNode);
+					closestNodes.add(subSetNode);
+					System.out.println("Found similar distance for "+openNode+", "+subSetNode+" ("+tmpDistance+")");
+				}
 			}
 		}
 		if (closestNodes.size() > 1) {
-			System.out.println("Closest nodes: "+closestNodes.get(0)+", "+closestNodes.get(1)+" ("+closestDistance+")");
+			System.out.println("Closest nodes: "+ closestNodes.toString() +" ("+closestDistance+")");
 		}
 		time = System.currentTimeMillis() - time;
 		System.out.println("Time in getClosestNode() single: " + (double)time / 1000.0);
