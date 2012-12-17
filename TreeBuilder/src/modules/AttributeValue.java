1dd640dcc0bed2491aa2e32f4377f81a8ee0db4b
alessandramacri <alessandra.macri@hotmail.com>
2012-12-17 03:58:07 -0800

    Create AttributeValue.java
    See last commit

---
+++ TreeBuilder/src/modules/AttributeValue.java
@@ -0,0 +1,12 @@
+package modules;
+
+/*Stores the occuring values for an attribute, including standard deviation, support, probability and rating.
+ * Calculation of these values must happen in the class using the AttributeValue. This class only provides the setters and getters. 
+ */
+
+public interface AttributeValue {
+
+  public void setParamteters(int support, double probability);
+	public void setSupport(int support);
+	public void setProbability(double probability);
+}
