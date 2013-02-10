2128cd1ce82f84800478bdf602f91845ff991289
alessandramacri <alessandra.macri@hotmail.com>
2013-02-10 17:44:50 +0100

    Test

--- TreeBuilder/test/ch/uzh/agglorecommender/client/TestClassFullTreeCobweb.java
+++ TreeBuilder/test/ch/uzh/agglorecommender/client/TestClassFullTreeCobweb.java
@@ -2,10 +2,11 @@ package ch.uzh.agglorecommender.client;
 
 
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Map.Entry;
 
 import org.junit.Test;
 
@@ -14,7 +15,6 @@ import ch.uzh.agglorecommender.clusterer.treecomponent.INode;
 import ch.uzh.agglorecommender.clusterer.treeupdate.NullUpdater;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.UnmodifiableIterator;
 
 public class TestClassFullTreeCobweb {
 	static String fileLocation = "test/ch/uzh/agglorecommender/client/testNodes.base";
@@ -47,27 +47,30 @@ public class TestClassFullTreeCobweb {
 	        e.printStackTrace();
 	    }
 		
-	
+		//Sleep test to allow to see visualization
+
 		System.out.println();
 		System.out.println("------------Retrieving values...------------");
 		
-		INode rootNode = trainingOutput.getUserTreeRoot();
+		//Checkk the leafes
 		ImmutableMap<Integer, INode> leafNodes = trainingOutput.getUserTreeLeavesMap();
 		int numberOfLeafes = leafNodes.size();
+		assertEquals("Number of leafes", 10, numberOfLeafes, 0.1);
 		
-		System.out.println("Number of leafes: "+numberOfLeafes);
+		//Check root node
+		INode rootNode = trainingOutput.getUserTreeRoot();
+		assertEquals("Root node utility",0.02125,rootNode.getCategoryUtility(),0.0000000001);
 		
 		//Get values from tree
-		INode tempNode;
-		
-		rootNode.getChildrenCount();
-		
-		
-		//Temporary solution
-		UnmodifiableIterator<Entry<Integer, INode>> entries = leafNodes.entrySet().iterator();
+		INode tempNode1;
+		INode tempNode2;
+
+		//Get values utilities of 1st merge level
+		/*UnmodifiableIterator<Entry<Integer, INode>> entries = leafNodes.entrySet().iterator();
 		while (entries.hasNext()) {
 		  Entry thisEntry = (Entry) entries.next();
 		  int key = (int) thisEntry.getKey();
+		  thisEntry.getValue();
 		  System.out.println("\nNode "+key+" has the following "+key+" nodes:");
 		  INode value = (INode)thisEntry.getValue();
 		  System.out.println("Is node: "+value.getAttributesString());
@@ -76,8 +79,7 @@ public class TestClassFullTreeCobweb {
 			  System.out.println("Number of attributes for node"+key);
 			  System.out.println(value.getAttributeKeys().size());
 		  }*/
-		  
-		}
+		//}
 		
 		//Temporary solution: Print 1st level merges
 		//System.out.println("Node: "+leafNodes.get(1).getAttributesString());
@@ -85,13 +87,13 @@ public class TestClassFullTreeCobweb {
 			//System.out.println("Node: "+leafNodes.get("1"));
 		//}
 		
-		//Give user time to see visualization before it is closed
-		System.out.println("\n You can pause the visualization now!");
+		System.out.println("\n Test done.. sleeping");
 		try {
 			Thread.sleep(500000000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	
 	}
 }
