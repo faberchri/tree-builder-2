d666d7c747888629dbc5cea0cef0f03aefb70191
alessandramacri <alessandra.macri@hotmail.com>
2013-02-14 18:35:25 +0100

    More work on decorator

--- TreeBuilder/src/ch/uzh/agglorecommender/clusterer/treesearch/RapidMinerClusterMaxCUSearcher.java
+++ TreeBuilder/src/ch/uzh/agglorecommender/clusterer/treesearch/RapidMinerClusterMaxCUSearcher.java
@@ -1,12 +1,16 @@
 package ch.uzh.agglorecommender.clusterer.treesearch;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import ch.uzh.agglorecommender.clusterer.treecomponent.INode;
 
+import com.rapidminer.example.ExampleSet;
+
 //This class is going to use RapidMiner to cluster the data. 
 //RapidMiner must be installed on computer (no source code needed, only installed program). 
-//Will use the RapidMinerDataTransformator to parse data to RapidMiner format
+//Uses RapidMinerDataTransformator to parse data to RapidMiner format
 
 public class RapidMinerClusterMaxCUSearcher extends MaxCategoryUtilitySearcherDecorator{
 	   
@@ -20,11 +24,34 @@ public class RapidMinerClusterMaxCUSearcher extends MaxCategoryUtilitySearcherDe
 	    }
 
 		@Override
-		public Set<IMergeResult> getMaxCategoryUtilityMerges(
-				Set<List<INode>> combinationsToCheck,
-				IClusterSet<INode> clusterSet) {
-			// TODO Auto-generated method stub
+		public Set<IMergeResult> getMaxCategoryUtilityMerges(Set<List<INode>> combinationsToCheck, IClusterSet<INode> clusterSet) {
+			//Transform combinationsToCheck into a RapidMiner-readable form
+			RapidMinerDataTransformator transformer = new RapidMinerDataTransformator();
+			Set<ExampleSet> oldExmapleSet = transformer.transform(combinationsToCheck);
+			
+			//Send to each ExampleSet to RapidMiner to cluster
+			ExampleSet tempSet;
+			Set<ExampleSet> newExampleSet = new HashSet();
+			Set<ExampleSet> tempExampleSet;
+			Iterator<ExampleSet> i = oldExmapleSet.iterator();
+			//Run clustering for each combination
+			while(i.hasNext()){
+				tempSet = i.next();
+				tempExampleSet = this.runRMclustering(tempSet);
+				Iterator<ExampleSet> j = tempExampleSet.iterator();
+				while(j.hasNext()){
+					tempSet = j.next();
+					newExampleSet.add(tempSet);
+				}				
+			}
+			//Return merge result
 			return null;
 		}
-	
+		
+		public Set<ExampleSet> runRMclustering(ExampleSet setToCluster){
+			
+			
+			
+			return null;
+		}
 }
