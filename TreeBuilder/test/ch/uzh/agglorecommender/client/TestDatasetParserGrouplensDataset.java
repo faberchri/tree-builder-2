e99d7e5a86ba7ea2461c6a164298005a1b329dab
alessandramacri <alessandra.macri@hotmail.com>
2013-02-24 17:12:12 +0100

    Tests

--- TreeBuilder/test/ch/uzh/agglorecommender/client/TestDatasetParserGrouplensDataset.java
+++ TreeBuilder/test/ch/uzh/agglorecommender/client/TestDatasetParserGrouplensDataset.java
@@ -11,8 +11,9 @@ import ch.uzh.agglorecommender.client.IDataset.DataSetSplit;
 /* Tests whether all data is loaded from Grouplens dataset*/
 public class TestDatasetParserGrouplensDataset {
 	
+	/*Test for the small dataset*/
 	@Test
-	public void runTest(){
+	public void grouplensSmall(){
 
 	//Test dataset	
 	int countTestDataset = 0;
@@ -32,7 +33,34 @@ public class TestDatasetParserGrouplensDataset {
 			 trainingIterator.next();
 		 }
 	
-	 assertEquals("Size of Grouplens test dataset",20000,countTestDataset,0.1);
-	 assertEquals("Size of Grouplens training dataset",80000,countTrainingDataset,0.1);
+	 assertEquals("Size of small Grouplens test dataset",20000,countTestDataset,0.1);
+	 assertEquals("Size of small Grouplens training dataset",80000,countTrainingDataset,0.1);
 	 
-}}
+}
+	/*Test for the big dataset*/
+	@Test
+public void grouplensBig(){
+		
+		//Test dataset
+	FloareaDataset testDataset = new FloareaDataset(null,DataSetSplit.TEST);
+	int countTestDataset = 0;
+	Iterator<IDatasetItem<Double>> testIterator = testDataset.iterateOverDatasetItems();
+	 while (testIterator.hasNext()){
+		 countTestDataset++;
+		 testIterator.next();
+	 }
+	 
+	 //Training dataset
+		int countTrainingDataset = 0;
+		FloareaDataset trainingDataset = new FloareaDataset(null,DataSetSplit.TRAINING);
+		Iterator<IDatasetItem<Double>> trainingIterator = trainingDataset.iterateOverDatasetItems();
+		 while (trainingIterator.hasNext()){
+			 countTrainingDataset++;
+			 trainingIterator.next();
+		 }
+	 //TODO: What is the correct size?
+		 assertEquals("Size of big Grouplens test dataset",20000,countTestDataset,0.1);
+		 assertEquals("Size of big Grouplens training dataset",80000,countTrainingDataset,0.1);
+}
+	
+}
