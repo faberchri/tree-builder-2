95645fab9b52935c3495477dae97487b0ecc5f81
alessandramacri <alessandra.macri@hotmail.com>
2013-02-26 17:45:00 +0100

    Commit to pull

---
+++ TreeBuilder/test/ch/uzh/agglorecommender/recommender/RankRecommendationTest.java
@@ -0,0 +1,8 @@
+package ch.uzh.agglorecommender.recommender;
+
+
+public class RankRecommendationTest {
+ //public ArrayList<IAttribute> rankRecommendation(Map<INode, IAttribute> unsortedRecommendation,int direction, int limit)
+	
+	
+}
