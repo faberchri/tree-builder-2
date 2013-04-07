ff74c4630aa600518a6a961bd1c045ced3642782
Daniel Hegglin <dani.hegglin@gmail.com>
2013-04-07 20:30:00 +0200

    Fixed Rank Recommendation Test

--- TreeBuilder/test/ch/uzh/agglorecommender/recommender/RankRecommendationTest.java
+++ TreeBuilder/test/ch/uzh/agglorecommender/recommender/RankRecommendationTest.java
@@ -51,18 +51,11 @@ public class RankRecommendationTest {
 		unsortedRecommendations.put(node4,att4);
 		
 		// Sort recommendations
-<<<<<<< HEAD
 		ClusterResult cr = new ClusterResult(null,null,null, null, UUID.randomUUID());
-		RecommendationModel ranker = new RecommendationModel(cr, null);
+		Recommender ranker = new Recommender(cr, null);
 		SortedMap<INode,IAttribute> sortedRecommendations = new TreeMap<INode, IAttribute>();
 		sortedRecommendations = ranker.rankRecommendation(unsortedRecommendations, 1, 4);
-=======
-		ClusterResult cr = new ClusterResult(null,null,null, null, null);
-		Recommender ranker = new Recommender(cr, null);
-		Map<INode,IAttribute> sortedRecommendations = new HashMap<INode, IAttribute>();
 		ranker.rankRecommendation(unsortedRecommendations, 1, 4);
->>>>>>> Proxy Pattern
-		
 		
 		// Check results
 		assertEquals("All nodes in map",unsortedRecommendations.size(),sortedRecommendations.size(),0.01);
